f635c0bebc0964c2f39793f59a64afbac860c39e
karta0807913 <karta0807913@gmail.com>
2021-12-27 08:53:40 +0800

    require dap-go and update container script

--- docker/update-config2all-docker.sh
+++ docker/update-config2all-docker.sh
@@ -10,6 +10,8 @@ else
     exit 127
 fi
 
+echo "Updating exists container..."
+
 containers="$(${CONTAINERD} container ls -a --format '{{ .Names }} {{ .Image }}' | grep 'my_editor' | awk '{print $1}')"
 
 for container in ${containers};
@@ -17,4 +19,18 @@ do
     echo "updating ${container}";
     ${CONTAINERD} cp "${HOME}/.emacs.d/init.el" "${container}:/home/code/.emacs.d";
     ${CONTAINERD} cp "${HOME}/.emacs.d/lisp" "${container}:/home/code/.emacs.d";
+done
+
+echo "
+Updating images..."
+
+images="$(${CONTAINERD} image list --format '{{ .Repository }}:{{ .Tag }}' | grep 'my_editor')"
+for image in ${images};
+do
+    echo "updating ${image}"
+    ${CONTAINERD} run --rm -it -d --name updating_container ${image} emacs -nw
+    ${CONTAINERD} cp "${HOME}/.emacs.d/init.el" "updating_container:/home/code/.emacs.d";
+    ${CONTAINERD} cp "${HOME}/.emacs.d/lisp" "updating_container:/home/code/.emacs.d";
+    ${CONTAINERD} commit updating_container ${image}
+    ${CONTAINERD} stop updating_container
 done
--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,7 +1,3 @@
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-
 (defun go-mode-lsp-custon-settings()
   (add-hook 'lsp-mode-hook (lambda ()
                             (lsp-register-custom-settings
@@ -10,12 +6,17 @@
                                 ("gopls.completeUnimported" t t))
                               ))))
 
+(defun go-mode-configure ()
+  (set-locale-environment compile-command)
+  (setq compile-command "go test ./...")
+  (eval-after-load 'dap-mode (progn  (require 'dap-go) (dap-go-setup))))
+
 (add-hook 'go-mode-hook #'flycheck-mode)
 (add-hook 'go-mode-hook #'go-mode-lsp-custon-settings)
 
 (add-hook 'go-mode-hook #'lsp-deferred)
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 (add-hook 'go-mode-hook #'yas-minor-mode)
-(add-hook 'go-mode-hook (lambda () (setq compile-command "go test ./...")))
+(add-hook 'go-mode-hook #'go-mode-configure)
 
 (provide 'init-go)
