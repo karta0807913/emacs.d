87e796d4f9538bfea4c9520110ac69b15968c812
Liang Chu-Xuan <chuxuan.liang@shopee.com>
2022-12-19 07:04:47 +0800

    fix small bug

--- lisp/init-xref.el
+++ lisp/init-xref.el
@@ -2,7 +2,7 @@
 (with-eval-after-load 'xref
   (define-key xref--xref-buffer-mode-map (kbd "j") 'xref-next-line)
   (define-key xref--xref-buffer-mode-map (kbd "k") 'xref-prev-line)
-  (define-key xref--xref-buffer-mode-map (kbd "RET") '(lambda ()
+  (define-key xref--xref-buffer-mode-map (kbd "RET") (lambda ()
                                                         (interactive)
                                                         (xref-goto-xref t))))
 
