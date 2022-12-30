27bf1e6b5d3ad67809a410655f808a3e109e3677
Liang Chu-Xuan <chuxuan.liang@shopee.com>
2022-12-30 17:21:24 +0800

    add globacl copy and past to simulate tui

--- lisp/init-graphic.el
+++ lisp/init-graphic.el
@@ -3,6 +3,9 @@
     (evil-define-key 'insert 'global (kbd "C-S-v") 'paste-from-x-clipboard)
     (evil-define-key 'visual 'global (kbd "C-S-c") 'copy-to-x-clipboard)
     (evil-define-key 'normal 'global "gp" 'preview-frame-preview))
+  (setq select-enable-clipboard nil)
+  (global-set-key (kbd "C-S-v") 'paste-from-x-clipboard)
+  (global-set-key (kbd "C-S-c") 'copy-to-x-clipboard)
   (require-init 'init-preview-frame))
 
 (provide 'init-graphic)
