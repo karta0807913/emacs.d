079cae0ed7501dcf752bc30885acb8a9fafcfe6c
karta0807913 <karta0807913@gmail.com>
2024-04-20 16:24:47 +0800

    add ginkgo test by using treesit

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,4 +1,40 @@
 (with-eval-after-load 'go-mode
+  (require 'treesit)
+
+  (defun go-mode-treesit-find-test-ginko-it-function-name-at-point ()
+    (when-let* ((p (treesit-ready-p 'go))
+                (call-expression
+                 (treesit-parent-until
+                  (treesit-node-at (point))
+                  (lambda (node)
+                    (when (string= (treesit-node-type node) "call_expression")
+                      (let ((expression (car (treesit-node-children node))))
+                        (and (string= (treesit-node-type expression) "identifier")
+                             (string= (treesit-node-text expression) "It")))))
+                  t))
+                ;; children is (identifier argument_list)
+                (children (treesit-node-children call-expression))
+                (argument_list (car (cdr children))))
+      (treesit-node-text (nth 1
+                              ;; children of argument_list
+                              (treesit-node-children (car (cdr children)))))))
+
+  (defun go-mode-regex-find-test-t-function-name-at-point ()
+    (save-excursion
+      (evil-backward-section-begin)
+      (let ((function-text (buffer-substring-no-properties
+                            (line-beginning-position) (line-end-position)))
+            (run-args nil))
+        (save-match-data
+          (and (string-match "func Test\\([^(]+\\)(t \\*testing.T)" function-text)
+               (match-string 1 function-text)
+               (setq run-args (format "-run Test%s" (match-string 1 function-text)))))
+        run-args)))
+
+  (defun go-mode-treesit-buffer-hook ()
+    (when (treesit-ready-p 'go)
+      (treesit-parser-create 'go)))
+
   (defun lsp-go-install-save-hooks ()
     (add-hook 'before-save-hook #'lsp-format-buffer t t)
     (add-hook 'before-save-hook #'lsp-organize-imports t t))
@@ -20,19 +56,19 @@
     :keymaps 'go-mode-map)
 
   (golang-leader-def
-   "op" (lambda ()
-          (interactive)
-          (save-excursion
-            (evil-backward-section-begin)
-            (let ((function-text (buffer-substring-no-properties
-                                  (line-beginning-position) (line-end-position)))
-                  (run-args "./..."))
-              (save-match-data
-                (and (string-match "func Test\\([^(]+\\)(t \\*testing.T)" function-text)
-                     (match-string 1 function-text)
-                     (setq run-args (format "-run Test%s" (match-string 1 function-text)))))
-              (setq compile-command (format "go test %s" run-args))))
-              (my-compile)))
+    "op" (lambda ()
+           (interactive)
+           (setq compile-command
+                 (let ((ginkgo-it (go-mode-treesit-find-test-ginko-it-function-name-at-point))
+                       (regex-t (go-mode-regex-find-test-t-function-name-at-point)))
+                   (cond
+                    (ginkgo-it
+                     (format "ginkgo --focus %s" ginkgo-it))
+                    (regex-t
+                     (format "go test %s" regex-t))
+                    (t
+                     "go test ./..."))))
+           (my-compile)))
 
   (add-hook 'go-mode-hook #'flycheck-mode)
   (add-hook 'go-mode-hook #'go-mode-lsp-custon-settings)
